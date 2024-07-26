f30b9b205314d1051fa3f9c5ce835e7f83bff2f0
Camila <camila.gomez@fceqyn.unam.edu.ar>
2024-07-26 16:47:05 -0300

    Creacion de paquetes de MVC y persistencia

--- src/main/java/unam/biblioteca/Biblioteca.java
+++ src/main/java/unam/biblioteca/Biblioteca.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package unam.biblioteca;
 
-/**
- *
- * @author camilaailen
- */
 public class Biblioteca {
 
     public static void main(String[] args) {
