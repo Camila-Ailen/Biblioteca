0638e04cd9d928d77d0fcbb66e1700a814fc5dd5
Camila <camila.gomez@fceqyn.unam.edu.ar>
2024-07-26 15:33:46 -0300

    Creacion de proyecto con Maven (sin JavaFX)

---
+++ src/main/java/unam/biblioteca/Biblioteca.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package unam.biblioteca;
+
+/**
+ *
+ * @author camilaailen
+ */
+public class Biblioteca {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
